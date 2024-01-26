47b35e788e24b68aa31aa9b0f949ebafb5ec217f
XUANJI233 <56026539+XUANJI233@users.noreply.github.com>
2024-01-26 13:47:41 +0800

    Update get_ip_utils.py

--- github_host/get_ip_utils.py
+++ github_host/get_ip_utils.py
@@ -1,53 +1,7 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-'''
-@Contact :   liuyuqi.gov@msn.cn
-@Time    :   2019/08/03 17:02:15
-@License :   Copyright © 2017-2022 liuyuqi. All Rights Reserved.
-@Desc    :   get ip from ip address
-'''
-
-from email import header
-import requests
-from bs4 import BeautifulSoup
-import re
-import json
 
 
-def getIpFromIpaddress(site):
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebkit/737.36(KHTML, like Gecke) Chrome/52.0.2743.82 Safari/537.36',
-               'Host': 'ipaddress.com'}
-    url = "https://ipaddress.com/search/" + site
-    trueip = None
-    try:
-        res = requests.get(url, headers=headers, timeout=5)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        ip = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", res.text)
-        result = soup.find_all('div', class_="comma-separated")
-        for c in result:
-            if len(ip) != 0:
-                trueip = ip[0]
-    except Exception as e:
-        print("查询" + site + " 时出现错误: " + str(e))
-    return trueip
 
-
-def getIpFromChinaz(site):
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebkit/737.36(KHTML, like Gecke) Chrome/52.0.2743.82 Safari/537.36',
-               'Host': 'ip.tool.chinaz.com'}
-    url = "http://ip.tool.chinaz.com/" + site
-    trueip = None
-    try:
-        res = requests.get(url, headers=headers, timeout=5)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        result = soup.find_all('span', class_="Whwtdhalf w15-0")
-        for c in result:
-            ip = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", c.text)
-            if len(ip) != 0:
-                trueip = ip[0]
-    except Exception as e:
-        print("查询" + site + " 时出现错误: " + str(e))
-    return trueip
 
 
 def getIpFromWhatismyipaddress(site):
@@ -64,12 +18,14 @@ def getIpFromWhatismyipaddress(site):
         soup = BeautifulSoup(res.text, 'html.parser')
         result = soup.find_all('span', class_="Whwtdhalf w15-0")
         for c in result:
-            ip = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", c.text)
+            ip = re.findall(r"\b(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}\b", c.text)
             if len(ip) != 0:
                 trueip = ip[0]
     except Exception as e:
         print("查询" + site + " 时出现错误: " + str(e))
     return trueip
+
+
 
 
 def getIpFromipapi(site):
@@ -84,9 +40,9 @@ def getIpFromipapi(site):
         try:
             res = requests.get(url, headers=headers, timeout=5)
             res = json.loads(res.text)
-            if(res["status"] == "success") and len(re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", res["query"])) == 1:
+            if(res["status"] == "success") and len(re.findall(r"\b(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}\b", res["query"])) == 1:
                 trueip = res["query"]
                 break
         except Exception as e:
             print("查询" + site + " 时出现错误: " + str(e))
-    return trueip
+    return trueipf
