61a0dd7b1bb55ad19500cff40620ba252c6b1022
XUANJI233 <56026539+XUANJI233@users.noreply.github.com>
2024-01-28 13:10:13 +0800

    Update github.py

--- github_host/github.py
+++ github_host/github.py
@@ -59,7 +59,7 @@ class Github(object):
                 f2.write("#*********************github " +
                      str(today) + " update********************\n")
                 for key in range(1, len(self.trueip), 2):
-                         f2.write(self.trueip[key] + "\t" + self.trueip[key+1] + "\n")
+                    f2.write(self.trueip[key] + "\t" + self.trueip[key+1] + "\n")
         os.remove(self.hostLocation)
         os.rename("temphost", self.hostLocation)
         # os.system("ipconfig /flushdns")
