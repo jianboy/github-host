4fe647b801f6a9d6c720196cad23864c56f7485c
XUANJI233 <56026539+XUANJI233@users.noreply.github.com>
2024-01-28 09:15:11 +0800

    Update github.py

--- github_host/github.py
+++ github_host/github.py
@@ -49,7 +49,9 @@ class Github(object):
         trueip =[]
         today = datetime.date.today()
         for site in self.sites:
-            trueip.extend(get_ip_utils.getIpFromipapi(site))
+            ips = get_ip_utils.getIpFromipapi(site)
+            if ips:  # 检查 ips 是否不为 None 且为非空列表
+                trueip.extend(ips)
         with open(self.hostLocation, "r") as f1:
             f1_lines = f1.readlines()
             with open("temphost", "w") as f2:
